ae5608157d89efa7e7651d3271547aabec04aa7b
heroyooi <heroyooi1018@gmail.com>
2024-03-12 23:46:54 +0900

    파이어베이스 firestore 연동

--- app/api/todos/[slug]/route.ts
+++ app/api/todos/[slug]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+// 할일 단일 조회
 export async function GET(
   request: NextRequest,
   { params }: { params: { slug: string } }
@@ -15,6 +16,42 @@ export async function GET(
       is_done: false,
       query,
     },
+  };
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 삭제 id
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const response = {
+    message: '단일 할일 삭제 성공',
+    data: {
+      id: params.slug,
+      title: '오늘도 빡코딩!',
+      is_done: false,
+    },
+  };
+  return NextResponse.json(response, { status: 200 });
+}
+
+// 할일 단일 수정 id
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const { title, is_done } = await request.json();
+
+  const editedTodo = {
+    id: params.slug,
+    title: title,
+    is_done,
+  };
+
+  const response = {
+    message: '단일 할일 수정 성공',
+    data: editedTodo,
   };
   return NextResponse.json(response, { status: 200 });
 }
---
+++ data/firestore.js
@@ -0,0 +1,63 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  Timestamp,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 모든 할일 가져오기
+export async function fetchTodos() {
+  const querySnapshot = await getDocs(collection(db, 'todos'));
+
+  if (querySnapshot.empty) {
+    return [];
+  }
+
+  const fetchedTodos = [];
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, ' => ', doc.data());
+    const aTodo = {
+      id: doc.id,
+      title: doc.data()['title'],
+      is_done: doc.data()['is_done'],
+      created_at: doc.data()['created_at'].toDate(),
+    };
+    // .toLocaleTimeString('ko')
+    fetchedTodos.push(aTodo);
+  });
+
+  return fetchedTodos;
+}
+
+// 할일 추가
+export async function addATodo({ title }) {
+  const newTodoRef = doc(collection(db, 'todos'));
+  const createdAtTimestamp = Timestamp.fromDate(new Date());
+  const newTodoData = {
+    id: newTodoRef.id,
+    title,
+    is_done: false,
+    created_at: createdAtTimestamp,
+  };
+  await setDoc(newTodoRef, newTodoData);
+
+  return newTodoData;
+}
+
+module.exports = { fetchTodos, addATodo };
